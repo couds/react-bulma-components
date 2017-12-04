7bca5a3e1a7999452a48637e1b2e400ee94c2da8
John Benavides <john@mailtrack.io>
2017-12-04 17:44:03 +0100

    update Icon component

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -6,50 +6,55 @@ import CONSTANTS from '../../constants';
 
 const colors = [null].concat(Object.keys(CONSTANTS.COLORS).map(key => CONSTANTS.COLORS[key]));
 
-const Icon = ({
-  icon,
-  size,
-  color,
-  className,
-  style,
-  align,
-  children,
-}) => (
-  <span
-    style={style}
-    className={classnames('icon', className, {
-      [`is-${size}`]: size,
-      [`is-${align}`]: align,
-      [`has-text-${color}`]: color,
-})}
-  >
-    { children || (
-      <i
-        className={classnames('rbc', {
-          [`rbc-${icon}`]: icon,
-        })}
-      />
-    )}
-  </span>
-);
 
-Icon.propTypes = {
-  icon: PropTypes.string.isRequired,
-  children: PropTypes.element,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  align: PropTypes.oneOf(['left', 'right']),
-  color: PropTypes.oneOf(colors),
-};
+export default class Icon extends PureComponent {
+  static propTypes = {
+    icon: PropTypes.string.isRequired,
+    children: PropTypes.element,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    align: PropTypes.oneOf(['left', 'right']),
+    color: PropTypes.oneOf(colors),
+  }
 
-Icon.defaultProps = {
-  className: '',
-  style: {},
-  size: null,
-  color: null,
-  children: null,
-  align: null,
-};
+  static defaultProps = {
+    className: '',
+    style: {},
+    size: null,
+    color: null,
+    children: null,
+    align: null,
+  }
 
-export default Icon;
+  render() {
+    const {
+      icon,
+      size,
+      color,
+      className,
+      style,
+      align,
+      children,
+    } = this.props;
+
+    return (
+      <span
+        style={style}
+        className={classnames('icon', className, {
+          [`is-${size}`]: size,
+          [`is-${align}`]: align,
+          [`has-text-${color}`]: color,
+    })}
+      >
+        { children || (
+          <i
+            className={classnames('rbc', {
+              [`rbc-${icon}`]: icon,
+            })}
+          />
+        )}
+      </span>
+    );
+  }
+}
