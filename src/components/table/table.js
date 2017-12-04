553cc8fd7bc4c143c2c60d2c6443f9191e4a9cb6
John Benavides <john@mailtrack.io>
2017-12-04 18:11:40 +0100

    update Table component

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,44 +1,46 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const Table = ({
-  children,
-  className,
-  style,
-  size,
-  striped,
-  bordered,
-}) => (
-  <table
-    style={style}
-    className={classnames('table', className, {
-      [`is-${size}`]: size,
-      'is-bordered': bordered,
-      'is-striped': striped,
+export default class Table extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    size: PropTypes.oneOf(['fullwidth', 'narrow']),
+    striped: PropTypes.bool,
+    bordered: PropTypes.bool,
+  }
 
-    })}
-  >
-    {children}
-  </table>
-);
+  static defaultProps = {
+    children: null,
+    className: '',
+    style: {},
+    size: 'fullwidth',
+    striped: true,
+    bordered: false,
+  }
 
-Table.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  size: PropTypes.oneOf(['fullwidth', 'narrow']),
-  striped: PropTypes.bool,
-  bordered: PropTypes.bool,
-};
-
-Table.defaultProps = {
-  children: null,
-  className: '',
-  style: {},
-  size: 'fullwidth',
-  striped: true,
-  bordered: false,
-};
-
-export default Table;
+  render() {
+    const {
+      children,
+      className,
+      style,
+      size,
+      striped,
+      bordered,
+    } = this.props;
+    return (
+      <table
+        style={style}
+        className={classnames('table', className, {
+          [`is-${size}`]: size,
+          'is-bordered': bordered,
+          'is-striped': striped,
+        })}
+      >
+        {children}
+      </table>
+    );
+  }
+}
