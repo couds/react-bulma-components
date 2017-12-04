92a0feb3b5026d04fa12abe5a2fb64b7d1528c4b
John Benavides <john@mailtrack.io>
2017-12-04 16:57:34 +0100

    update Card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,49 +1,51 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 import CardImage from './components/image';
 
-const Card = ({
-  className,
-  children,
-  style,
-  type,
-  renderAs,
-  ...props
-}) => {
-  if (type === 'image') {
-    return <CardImage {...props} style={style} className={className} />;
+export default class Card extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    style: PropTypes.object,
+    renderAs: PropTypes.string,
+    type: PropTypes.oneOf(['header', 'header-item', 'header-icon', 'footer', 'footer-item', 'image', 'content']),
   }
-  const Element = renderAs;
-  return (
-    <Element
-      style={style}
-      className={classnames(className, {
-        [`card-${type}`]: type,
-        card: !type,
-      })}
-      {...props}
-    >
-      {children}
-    </Element>
-  );
-};
 
-Card.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-  style: PropTypes.object,
-  renderAs: PropTypes.string,
-  type: PropTypes.oneOf(['header', 'header-item', 'header-icon', 'footer', 'footer-item', 'image', 'content']),
-};
+  static defaultProps = {
+    className: '',
+    children: null,
+    style: {},
+    type: null,
+    renderAs: 'div',
+  }
 
-Card.defaultProps = {
-  className: '',
-  children: null,
-  style: {},
-  type: null,
-  renderAs: 'div',
-};
+  render() {
+    const {
+      className,
+      children,
+      style,
+      type,
+      renderAs,
+      ...props
+    } = this.props;
+    if (type === 'image') {
+      return <CardImage {...props} style={style} className={className} />;
+    }
+    const Element = renderAs;
+    return (
+      <Element
+        style={style}
+        className={classnames(className, {
+          [`card-${type}`]: type,
+          card: !type,
+        })}
+        {...props}
+      >
+        {children}
+      </Element>
+    );
+  }
+}
 
-export default Card;
