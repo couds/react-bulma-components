a1fe88c163e8c0a184b3a4bd265e204fb7d1cae7
John Benavides <john@mailtrack.io>
2017-12-04 18:07:25 +0100

    update Section component

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,41 +1,42 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const Section = ({
-  children,
-  className,
-  style,
-  renderAs,
-  size,
-}) => {
-  const Element = renderAs;
-  return (
-    <Element
-      style={style}
-      className={classnames('section', className, {
-        [`is-${size}`]: size,
-      })}
-    >
-      {children}
-    </Element>
-  );
-};
+export default class Section extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    renderAs: PropTypes.string,
+    size: PropTypes.oneOf(['medium', 'large']),
+  }
 
-Section.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  renderAs: PropTypes.string,
-  size: PropTypes.oneOf(['medium', 'large']),
-};
+  static defaultProps = {
+    children: null,
+    className: '',
+    style: {},
+    renderAs: 'div',
+    size: null,
+  }
 
-Section.defaultProps = {
-  children: null,
-  className: '',
-  style: {},
-  renderAs: 'div',
-  size: null,
-};
-
-export default Section;
+  render() {
+    const {
+      children,
+      className,
+      style,
+      renderAs,
+      size,
+    } = this.props;
+    const Element = renderAs;
+    return (
+      <Element
+        style={style}
+        className={classnames('section', className, {
+          [`is-${size}`]: size,
+        })}
+      >
+        {children}
+      </Element>
+    );
+  }
+}
