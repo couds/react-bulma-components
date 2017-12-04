5c30e4b4d5714cbd20758db46b6f8b2ae77065a6
John Benavides <john@mailtrack.io>
2017-12-04 17:05:54 +0100

    update Container component

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -1,48 +1,49 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import CONSTANTS from '../../constants';
 
 const breakpoints = [null].concat(Object.keys(CONSTANTS.BREAKPOINTS).map(key => CONSTANTS.BREAKPOINTS[key]));
 
-const Container = ({
-  children,
-  fluid,
-  breakpoint,
-  className,
-  style,
-  renderAs,
-}) => {
-  const Element = renderAs;
-  return (
-    <Element
-      style={style}
-      className={classnames('container', className, {
-        'is-fluid': fluid,
-        [`is-${breakpoint}`]: breakpoint,
-      })}
-    >
-      {children}
-    </Element>
-  );
-};
+export default class Container extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+    fluid: PropTypes.bool,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    breakpoint: PropTypes.oneOf(breakpoints),
+    renderAs: PropTypes.string,
+  }
 
-Container.propTypes = {
-  children: PropTypes.node,
-  fluid: PropTypes.bool,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  breakpoint: PropTypes.oneOf(breakpoints),
-  renderAs: PropTypes.string,
-};
+  static defaultProps = {
+    fluid: false,
+    children: null,
+    breakpoint: null,
+    className: '',
+    style: {},
+    renderAs: 'div',
+  }
 
-Container.defaultProps = {
-  fluid: false,
-  children: null,
-  breakpoint: null,
-  className: '',
-  style: {},
-  renderAs: 'div',
-};
-
-export default Container;
+  render() {
+    const {
+      children,
+      fluid,
+      breakpoint,
+      className,
+      style,
+      renderAs,
+    } = this.props;
+    const Element = renderAs;
+    return (
+      <Element
+        style={style}
+        className={classnames('container', className, {
+          'is-fluid': fluid,
+          [`is-${breakpoint}`]: breakpoint,
+        })}
+      >
+        {children}
+      </Element>
+    );
+  }
+}
