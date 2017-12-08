18f10f48a5b95a468626fe7e61f70d6d8c2a14ed
John Benavides <john@mailtrack.io>
2017-12-08 19:13:12 +0100

    Use relative import on navbar for service

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 // eslint-disable-next-line
-import canUseDOM from 'services/can-use-dom';
+import canUseDOM from '../../services/can-use-dom';
 import Brand from './components/brand';
 import Burger from './components/burger';
 import Menu from './components/menu';
