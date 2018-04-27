cf4e6cc9e9c669cc613e90ae35ab6a490fc0780f
John Benavides <john@mailtrack.io>
2018-04-27 15:47:21 +0200

    Add new image sizes

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -8,7 +8,7 @@ export default class Image extends PureComponent {
     src: PropTypes.string,
     alt: PropTypes.string,
     style: PropTypes.shape({}),
-    size: PropTypes.oneOf([16, 24, 32, 48, 64, 96, 128, 'square', '1by1', '4by3', '3by2', '16by9', '2by1']),
+    size: PropTypes.oneOf([16, 24, 32, 48, 64, 96, 128, 'square', '1by1', '4by3', '3by2', '16by9', '2by1', '5by4', '5by3', '3by1', '4by5', '3by4', '2by3', '3by5', '9by16', '1by2', '1by3']),
     fallback: PropTypes.string,
   }
 
