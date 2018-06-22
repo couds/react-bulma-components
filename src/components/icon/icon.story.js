ccc1b9103ea35bb231fa428962cf8fc8340ac8d0
Onur Laru <larugit@false.one>
2018-06-22 18:31:26 +0200

    Icon doc update (#51)
    * Added example for Font Awesome
    
    * Added example for Font Awesome

--- src/components/icon/icon.story.js
+++ src/components/icon/icon.story.js
@@ -6,21 +6,38 @@ import { withInfo } from '@storybook/addon-info';
 import Icon from '.';
 
 storiesOf('Icon', module)
-  .addDecorator(story => (
-    <div style={{ margin: 10 }}>
-      {story()}
-    </div>
-  ))
-  .add('Default', withInfo()(() => (
-    <div>
-      <Icon icon="bars" color="info" />
-      <Icon icon="angle-down" />
-    </div>
-  )))
-  .add('Custom Icon', withInfo()(() => (
-    <div>
-      <Icon>
-        <span className="rbc rbc-bars" />
-      </Icon>
-    </div>
-  )));
+  .addDecorator(story => <div style={{ margin: 10 }}>{story()}</div>)
+  .add(
+    'Default',
+    withInfo()(() => (
+      <div>
+        <Icon icon="bars" color="info" />
+        <Icon icon="angle-down" />
+      </div>
+    )),
+  )
+  .add(
+    'Custom Icon',
+    withInfo({
+      text: `
+        Notice: Icon component only prepares space for the icon. You need to import the icon library of your choice extra. See the example below for Font Awesome:
+
+        ~~~js
+          import Icon from 'react-bulma-components/lib/components/icon';
+        
+          import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+          import faUserPlus from '@fortawesome/fontawesome-free-solid/faUserPlus';
+          
+          <Icon>
+            <FontAwesomeIcon icon={faUserPlus} />
+          </Icon>
+        ~~~
+      `,
+    })(() => (
+      <div>
+        <Icon>
+          <span className="rbc rbc-bars" />
+        </Icon>
+      </div>
+    )),
+  );
