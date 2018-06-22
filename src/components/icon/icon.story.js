cf953bef3183edcf9cba7d416feaef10870b0490
Onur Laru <larugit@false.one>
2018-06-22 17:25:51 +0200

    Added example for Font Awesome

--- src/components/icon/icon.story.js
+++ src/components/icon/icon.story.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import { storiesOf } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
-import Icon from ".";
+import Icon from '.';
 
-storiesOf("Icon", module)
+storiesOf('Icon', module)
   .addDecorator(story => <div style={{ margin: 10 }}>{story()}</div>)
   .add(
-    "Default",
+    'Default',
     withInfo()(() => (
       <div>
         <Icon icon="bars" color="info" />
         <Icon icon="angle-down" />
       </div>
-    ))
+    )),
   )
   .add(
-    "Custom Icon",
+    'Custom Icon',
     withInfo({
       text: `
         Notice: Icon component only prepares space for the icon. You need to import the icon library of your choice extra. See the example below for Font Awesome:
@@ -32,12 +32,12 @@ storiesOf("Icon", module)
             <FontAwesomeIcon icon={faUserPlus} />
           </Icon>
         ~~~
-      `
+      `,
     })(() => (
       <div>
         <Icon>
           <span className="rbc rbc-bars" />
         </Icon>
       </div>
-    ))
+    )),
   );
