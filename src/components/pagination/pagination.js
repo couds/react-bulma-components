f87d4e21b55fbdf1f67fdd4e51c8390609fada33
HaFLiNGeR <jean-charles.b@domraider.com>
2018-06-25 14:52:21 +0200

    Add pagination ellipsis

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -21,7 +21,7 @@ class Pagination extends React.PureComponent {
      * Classname of the container of the pagination, this could be used to customize the inner views
      */
     className: PropTypes.string,
-  }
+  };
 
   static defaultProps = {
     total: 1,
@@ -33,32 +33,32 @@ class Pagination extends React.PureComponent {
     showPrevNext: true,
     autoHide: true,
     className: '',
-  }
+  };
 
   goToPage = page => (evt) => {
     evt.preventDefault();
     this.props.onChange(page);
-  }
+  };
 
   firstPage = (current, total) => {
     if (current === 1) {
       return 1;
     }
 
-    const page = current - (this.props.delta * (current === total ? 2 : 1));
+    const page = current - this.props.delta * (current === total ? 2 : 1);
 
     return page <= 0 ? 1 : page;
-  }
+  };
 
   lastPage = (current, total) => {
     if (current === total) {
       return total;
     }
 
-    const page = current + (this.props.delta * (current === 1 ? 2 : 1));
+    const page = current + this.props.delta * (current === 1 ? 2 : 1);
 
     return page > total ? total : page;
-  }
+  };
 
   render() {
     const {
@@ -79,7 +79,9 @@ class Pagination extends React.PureComponent {
 
     if (current > total) {
       // eslint-disable-next-line no-console
-      console.warn('Pagination controller: Current page can not be greater than total');
+      console.warn(
+        'Pagination controller: Current page can not be greater than total',
+      );
       return null;
     }
 
@@ -92,56 +94,94 @@ class Pagination extends React.PureComponent {
         className={classnames('pagination', className)}
         aria-label="pagination"
       >
-        {
-          showPrevNext &&
-            <React.Fragment>
-              <a
-                role="button"
-                tabIndex={0}
-                onClick={current === 1 ? undefined : this.goToPage(current - 1)}
-                className="pagination-previous"
-                title="This is the first page"
-                disabled={current === 1}
-              >
-                {previous}
-              </a>
-              <a
-                role="button"
-                tabIndex={0}
-                onClick={current === total ? undefined : this.goToPage(current + 1)}
-                className="pagination-next"
-                disabled={current === total}
-              >
-                {next}
-              </a>
-            </React.Fragment>
-        }
-        {
-          delta > 0 &&
-            <React.Fragment>
-              <ul className="pagination-list">
-                {
-                  Array((lastPage - firstPage) + 1).fill(0).map((_, i) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <li key={i + firstPage}>
+        {showPrevNext && (
+          <React.Fragment>
+            <a
+              role="button"
+              tabIndex={0}
+              onClick={current === 1 ? undefined : this.goToPage(current - 1)}
+              className="pagination-previous"
+              title="This is the first page"
+              disabled={current === 1}
+            >
+              {previous}
+            </a>
+            <a
+              role="button"
+              tabIndex={0}
+              onClick={
+                current === total ? undefined : this.goToPage(current + 1)
+              }
+              className="pagination-next"
+              disabled={current === total}
+            >
+              {next}
+            </a>
+          </React.Fragment>
+        )}
+        {delta > 0 && (
+          <React.Fragment>
+            <ul className="pagination-list">
+              {current !== 1 &&
+                current > delta + 1 && (
+                  <React.Fragment>
+                    <li>
                       <a
-                        role="button"
-                        tabIndex={0}
-                        className={classnames('pagination-link', {
-                          'is-current': current === i + firstPage,
-                        })}
-                        onClick={current === firstPage + i ? undefined : this.goToPage(firstPage + i)}
-                        aria-label={`Page ${i + firstPage}`}
-                        aria-current="page"
+                        className="pagination-link"
+                        aria-label="Goto page 1"
+                        onClick={this.goToPage(1)}
                       >
-                        {i + firstPage}
+                        1
                       </a>
                     </li>
-                    ))
-                }
-              </ul>
-            </React.Fragment>
-        }
+                    <li>
+                      <span className="pagination-ellipsis">&hellip;</span>
+                    </li>
+                  </React.Fragment>
+                )}
+              {Array(lastPage - firstPage + 1)
+                .fill(0)
+                .map((_, i) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <li key={i + firstPage}>
+                    <a
+                      role="button"
+                      tabIndex={0}
+                      className={classnames('pagination-link', {
+                        'is-current': current === i + firstPage,
+                      })}
+                      onClick={
+                        current === firstPage + i
+                          ? undefined
+                          : this.goToPage(firstPage + i)
+                      }
+                      aria-label={`Page ${i + firstPage}`}
+                      aria-current="page"
+                    >
+                      {i + firstPage}
+                    </a>
+                  </li>
+                ))}
+              {current !== total &&
+                current + delta < total && (
+                  <React.Fragment>
+                    <li>
+                      <span className="pagination-ellipsis">&hellip;</span>
+                    </li>
+                    <li>
+                      <a
+                        className="pagination-link"
+                        aria-label={`Goto page ${total}`}
+                        onClick={this.goToPage(total)}
+                      >
+                        {total}
+                      </a>
+                    </li>
+                  </React.Fragment>
+                )}
+            </ul>
+          </React.Fragment>
+        )}
       </nav>
     );
   }
