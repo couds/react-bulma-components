41282542d7cca06ac2a25138949446d6f5397b2d
John Benavides <john@mailtrack.io>
2018-07-05 16:34:42 +0200

    create component Element as a dummy component to use bulma helpers

---
+++ src/components/element/__test__/element.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import renderer from 'react-test-renderer';
+import Element from '..';
+
+describe('Element component', () => {
+  it('Should Exist', () => {
+    expect(Element).toMatchSnapshot();
+  });
+  it('Should have helpers classnames', () => {
+    const component = renderer.create(
+      <Element textColor="white" pull="left">
+        Facebook
+      </Element>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  it('Should accept a react Element as renderAs prop', () => {
+    // eslint-disable-next-line react/prop-types
+    const Custom = props => (
+      <p {...props}>
+        Custom
+        {props.children}
+      </p>
+    );
+
+    Custom.propTypes = { children: PropTypes.node.isRequired };
+
+    const component = renderer.create(
+      <Element renderAs={Custom}>
+This should be a p element
+      </Element>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
