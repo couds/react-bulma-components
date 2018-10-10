4df317eb4580063cfa4ce95ded6709c03b3cab53
John Benavides <john.elric@gmail.com>
2018-10-10 19:14:28 +0200

    fix storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,6 @@ import { setOptions } from '@storybook/addon-options';
 import { withInfo } from '@storybook/addon-info';
 import { select, withKnobs } from '@storybook/addon-knobs';
 import { configureViewport, INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-import { withBackgrounds } from '@storybook/addon-backgrounds';
 import extraViewports from './extra-viewports.json';
 
 import 'index.sass';
@@ -15,13 +14,7 @@ setOptions({
   showAddonPanel: true,
 });
 
-
 addDecorator(withKnobs);
-
-addDecorator(withBackgrounds([
-  { name: 'twitter', value: '#00aced' },
-  { name: 'facebook', value: '#3b5998' },
-]));
 
 addDecorator(withInfo({
   source: false,
