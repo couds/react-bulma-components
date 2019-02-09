61b06e11b7890aa4a8f6c1387496909363e16a5c
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-09 17:17:57 +0100

    chore: fix code style in Progress storybook

--- src/components/progress/progress.story.js
+++ src/components/progress/progress.story.js
@@ -2,18 +2,20 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { Progress } from '../progress';
 
-storiesOf('Progress', module).add('Default', () => (
-  <div>
-    <Progress max={100} value={15} color="primary" size="small" />
-    <Progress max={100} value={15} color="info" />
-    <Progress max={100} value={15} color="success" size="medium" />
-    <Progress max={100} value={15} color="error" size="large" />
-  </div>
-)).add('Indeterminate', () => (
-  <div>
-    <Progress max={100} color="primary" size="small" />
-    <Progress max={100} color="info" />
-    <Progress max={100} color="success" size="medium" />
-    <Progress max={100} color="error" size="large" />
-  </div>
-));
+storiesOf('Progress', module)
+  .add('Default', () => (
+    <div>
+      <Progress max={100} value={15} color="primary" size="small" />
+      <Progress max={100} value={15} color="info" />
+      <Progress max={100} value={15} color="success" size="medium" />
+      <Progress max={100} value={15} color="error" size="large" />
+    </div>
+  ))
+  .add('Indeterminate', () => (
+    <div>
+      <Progress max={100} color="primary" size="small" />
+      <Progress max={100} color="info" />
+      <Progress max={100} color="success" size="medium" />
+      <Progress max={100} color="error" size="large" />
+    </div>
+  ));
