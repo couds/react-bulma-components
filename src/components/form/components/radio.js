24993e681fed819ad7879b32ea9b7dd3da475b7a
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-09 12:05:45 +0100

    feat: new radio component

--- src/components/form/components/radio.js
+++ src/components/form/components/radio.js
@@ -1,15 +1,30 @@
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import CONSTANTS from '../../../constants';
 import modifiers from '../../../modifiers';
 
+import './radio.sass';
+
+const colors = Object.values(CONSTANTS.COLORS);
+
 export const Radio = React.forwardRef(
-  ({ className, style, disabled, checked, value, name, children, ...allProps }, ref) => {
+  ({ className, style, disabled, value, children, checked, color, size, name, ...allProps }, ref) => {
     const props = modifiers.clean(allProps);
     return (
-      <label disabled={disabled} className={cn('radio', modifiers.getClassName(allProps), className)} style={style}>
-        <input {...props} ref={ref} name={name} checked={checked} type="radio" value={value} disabled={disabled} />
-        {children}
+      <label
+        disabled={disabled}
+        className={cn('b-radio radio', modifiers.getClassName(allProps), className)}
+        style={style}
+      >
+        <input {...props} ref={ref} name={name} type="radio" value={value} disabled={disabled} checked={checked} />
+        <span
+          className={cn('check', {
+            [`is-${color}`]: color,
+            [`is-${size}`]: size
+          })}
+        />
+        <span className="control-label">{children}</span>
       </label>
     );
   }
@@ -19,14 +34,16 @@ Radio.propTypes = {
   ...modifiers.propTypes,
   children: PropTypes.node,
   className: PropTypes.string,
+  style: PropTypes.shape({}),
+  color: PropTypes.oneOf(colors),
+  size: PropTypes.oneOf(Object.values(CONSTANTS.SIZES)),
+  disabled: PropTypes.bool,
+  value: PropTypes.string,
+  checked: PropTypes.bool,
   /**
    * The name of the input field Commonly used for [multi-input handling](https://reactjs.org/docs/forms.html#handling-multiple-inputs)
    */
-  name: PropTypes.string.isRequired,
-  style: PropTypes.shape({}),
-  disabled: PropTypes.bool,
-  checked: PropTypes.bool,
-  value: PropTypes.string
+  name: PropTypes.string
 };
 
 Radio.defaultProps = {
@@ -35,6 +52,9 @@ Radio.defaultProps = {
   className: '',
   value: '',
   style: {},
+  color: null,
+  size: null,
   disabled: false,
-  checked: false
+  checked: false,
+  name: null
 };
