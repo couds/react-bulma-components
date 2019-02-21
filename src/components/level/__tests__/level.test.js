8643c599eab08e01afc2713c734425689cbbf3d2
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-21 12:28:50 +0100

    refactoring: use react-testing-library for testing in level component

--- src/components/level/__tests__/level.test.js
+++ src/components/level/__tests__/level.test.js
@@ -1,43 +1,40 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from 'react-testing-library';
 import { Level } from '..';
 
 describe('Level component', () => {
-  it('should exist', () => {
-    expect(Level).toMatchSnapshot();
-  });
   it('should expose Level Side and Item', () => {
-    expect(Level.Side).toMatchSnapshot();
-    expect(Level.Item).toMatchSnapshot();
+    expect(Level.Side).toBeDefined();
+    expect(Level.Item).toBeDefined();
   });
   it('should have level classname', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Level>
         <Level.Item>Item 1</Level.Item>
         <Level.Item>Item 2</Level.Item>
         <Level.Item>Item 3</Level.Item>
       </Level>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should concat classname in props with Bulma classname', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Level className="other-class this-is-a-test">
         <p>Default</p>
       </Level>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should use inline styles', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Level style={{ height: 250 }}>
         <p>Default</p>
       </Level>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should render Levels with right and left side', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Level>
         <Level.Side>
           <Level.Item>Item 1</Level.Item>
@@ -48,6 +45,6 @@ describe('Level component', () => {
         </Level.Side>
       </Level>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
