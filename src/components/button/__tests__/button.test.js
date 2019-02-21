cbf97a2ddae5c8b977e116fc179d297085f0d51f
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-21 08:43:12 +0100

    refactoring: use react-testing-library for testing in button component

--- src/components/button/__tests__/button.test.js
+++ src/components/button/__tests__/button.test.js
@@ -1,7 +1,6 @@
-import { mount, shallow } from 'enzyme';
+import { render, fireEvent } from 'react-testing-library';
 import PropTypes from 'prop-types';
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { Button } from '..';
 
 const Link = ({ to, children }) => <a href={to}>{children}</a>;
@@ -12,126 +11,123 @@ Link.propTypes = {
 };
 
 describe('Button component', () => {
-  it('Should exist', () => {
-    expect(Button).toMatchSnapshot();
-  });
   it('Should expose Button Group', () => {
     expect(Button.Group).toMatchSnapshot();
   });
   it('Should be a default Button', () => {
-    const component = renderer.create(<Button />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should be an anchor button', () => {
-    const component = renderer.create(<Button renderAs="a" href="https://github.com/couds/react-bulma-components" />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button renderAs="a" href="https://github.com/couds/react-bulma-components" />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should be a Primary Button', () => {
-    const component = renderer.create(<Button color="primary" />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button color="primary" />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should be a Large Primary Button', () => {
-    const component = renderer.create(<Button color="primary" size="large" />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button color="primary" size="large" />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should render as a static Button', () => {
-    const component = renderer.create(<Button isStatic color="primary" />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button isStatic color="primary" />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should render as a html button', () => {
-    const component = renderer.create(<Button renderAs="button" color="danger" />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button renderAs="button" color="danger" />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should render as a React element link with to prop', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Button renderAs={Link} to="http://google.com" color="danger">
         TEST
       </Button>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should render be disabled', () => {
-    const component = renderer.create(<Button disabled />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button disabled />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should be a submit form button', () => {
-    const component = renderer.create(<Button submit />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button submit />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should be a reset form button', () => {
-    const component = renderer.create(<Button reset />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Button reset />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Should have a Click listener', () => {
     const onClick = jest.fn();
-    const component = shallow(<Button onClick={onClick} />);
-    component.simulate('click');
+    const { container } = render(<Button onClick={onClick} />);
+    fireEvent.click(container.firstChild);
     expect(onClick).toHaveBeenCalledTimes(1);
   });
   it('Should have no dispatch click handler if disabled', () => {
     const onClick = jest.fn();
-    const component = shallow(<Button disabled onClick={onClick} />);
-    component.simulate('click');
+    const { container } = render(<Button disabled onClick={onClick} />);
+    fireEvent.click(container.firstChild);
     expect(onClick).toHaveBeenCalledTimes(0);
   });
   it('Should have a call default onClick is no listener is set', () => {
     const spy = jest.spyOn(Button.defaultProps, 'onClick');
-    const component = shallow(<Button />);
-    component.simulate('click');
-    component.simulate('click');
+    const { container } = render(<Button />);
+    fireEvent.click(container.firstChild);
+    fireEvent.click(container.firstChild);
     expect(spy).toHaveBeenCalledTimes(2);
     Button.defaultProps.onClick.mockRestore();
   });
   it('Should forward ref', () => {
     const testRef = React.createRef();
-    mount(<Button ref={testRef} />);
+    render(<Button ref={testRef} />);
     expect(testRef.current).not.toBeNull();
   });
   describe('Button Group component', () => {
     it('Should be a default list of buttons', () => {
-      const component = renderer.create(
+      const { asFragment } = render(
         <Button.Group>
           <Button>test 0</Button>
           <Button>test 1</Button>
         </Button.Group>
       );
-      expect(component).toMatchSnapshot();
+      expect(asFragment()).toMatchSnapshot();
     });
     it('Should concat class names in props with Bulma class name', () => {
-      const component = renderer.create(
+      const { asFragment } = render(
         <Button.Group className="super-class-1 dope-class-2">
           <Button>test 0</Button>
           <Button>test 1</Button>
         </Button.Group>
       );
-      expect(component.toJSON()).toMatchSnapshot();
+      expect(asFragment()).toMatchSnapshot();
     });
     it('Should group buttons together', () => {
-      const component = renderer.create(
+      const { asFragment } = render(
         <Button.Group hasAddons>
           <Button>test 1</Button>
           <Button>test 2</Button>
         </Button.Group>
       );
-      expect(component.toJSON()).toMatchSnapshot();
+      expect(asFragment()).toMatchSnapshot();
     });
     it('Should be centered', () => {
-      const component = renderer.create(
+      const { asFragment } = render(
         <Button.Group position="centered">
           <Button>test 0</Button>
           <Button>test 1</Button>
         </Button.Group>
       );
-      expect(component.toJSON()).toMatchSnapshot();
+      expect(asFragment()).toMatchSnapshot();
     });
     it('Should align to the right', () => {
-      const component = renderer.create(
+      const { asFragment } = render(
         <Button.Group position="right">
           <Button>test 0</Button>
           <Button>test 1</Button>
         </Button.Group>
       );
-      expect(component.toJSON()).toMatchSnapshot();
+      expect(asFragment()).toMatchSnapshot();
     });
   });
 });
