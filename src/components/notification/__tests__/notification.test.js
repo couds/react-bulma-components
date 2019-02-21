2efd5e4bb27bd1bf14da6e9250f98264b2fa6706
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-21 14:37:16 +0100

    refactoring: use react-testing-library for testing in notification component

--- src/components/notification/__tests__/notification.test.js
+++ src/components/notification/__tests__/notification.test.js
@@ -1,52 +1,53 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from 'react-testing-library';
 import { Notification } from '..';
 import CONSTANTS from '../../../constants';
 
 describe('Notification component', () => {
-  it('should exist', () => {
-    expect(Notification).toMatchSnapshot();
+  it('should render', () => {
+    const { asFragment } = render(<Notification />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should have notification classname', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Notification>
         <img alt="placeholder" src="http://bulma.io/images/placeholders/128x128.png" />
       </Notification>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should concat classname in props with classname', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Notification className="other-class this-is-a-test">
         <p>Default</p>
       </Notification>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should use inline styles', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Notification style={{ height: 250 }}>
         <p>Default</p>
       </Notification>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should render as Section', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Notification renderAs="section">
         <p>Default</p>
       </Notification>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   Object.values(CONSTANTS.COLORS).map(color =>
     it(`Should use use color ${color}`, () => {
-      const component = renderer.create(
+      const { asFragment } = render(
         <Notification color={color}>
           <p>Default</p>
         </Notification>
       );
-      expect(component.toJSON()).toMatchSnapshot();
+      expect(asFragment()).toMatchSnapshot();
     })
   );
 });
