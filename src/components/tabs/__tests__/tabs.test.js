57d7ce0c38a324403b083d99d691b3fa1a51e7ed
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-21 15:05:57 +0100

    refactoring: use react-testing-library for testing in tabs component

--- src/components/tabs/__tests__/tabs.test.js
+++ src/components/tabs/__tests__/tabs.test.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from 'react-testing-library';
 import { Tabs } from '..';
 
 describe('Tabs component', () => {
-  it('should Exist', () => {
-    expect(Tabs).toMatchSnapshot();
-  });
-  it('should expose Tab', () => {
-    expect(Tabs.Tab).toMatchSnapshot();
-  });
-  it('should have Tabs classname', () => {
-    const component = renderer.create(<Tabs>Facebook</Tabs>);
-    expect(component.toJSON()).toMatchSnapshot();
+  it.each([[Tabs], [Tabs.Tab]])('should render', Component => {
+    const { asFragment } = render(<Component>Content</Component>);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should concat classname in props with classname', () => {
-    const component = renderer.create(<Tabs className="other-class test">Facebook</Tabs>);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Tabs className="other-class test" />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should render as an html section', () => {
-    const component = renderer.create(<Tabs renderAs="section">This should be a section</Tabs>);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Tabs renderAs="section">This should be a section</Tabs>);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should have custom inline styles', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Tabs renderAs="section" style={{ width: 200, zIndex: 1 }}>
         This should be a section with custom styles
       </Tabs>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should accept a react Element as renderAs prop', () => {
     // eslint-disable-next-line react/prop-types
@@ -37,17 +31,17 @@ describe('Tabs component', () => {
         {children}
       </p>
     );
-    const component = renderer.create(<Tabs renderAs={Custom}>This should be a p element</Tabs>);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Tabs renderAs={Custom}>This should be a p element</Tabs>);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should render propertly Inside Tabs', () => {
-    const component = renderer.create(
+    const { asFragment } = render(
       <Tabs>
         <Tabs.Tab>Tab 1</Tabs.Tab>
         <Tabs.Tab>Tab 2</Tabs.Tab>
         <Tabs.Tab>Tab 3</Tabs.Tab>
       </Tabs>
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
