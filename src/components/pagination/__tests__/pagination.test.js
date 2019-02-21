da462a939e3f9847b45ffc41fb880a696dfea5e2
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-02-21 14:41:21 +0100

    refactoring: use react-testing-library for testing in pagination component

--- src/components/pagination/__tests__/pagination.test.js
+++ src/components/pagination/__tests__/pagination.test.js
@@ -1,51 +1,44 @@
-import { mount } from 'enzyme';
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { fireEvent, render } from 'react-testing-library';
 import { Pagination } from '..';
 
 describe('Pagination component', () => {
-  it('Pagination Should Exist', () => {
-    expect(Pagination).toMatchSnapshot();
-  });
   it('should have 3 pages and page 1 active', () => {
-    const component = renderer.create(<Pagination total={3} current={1} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination total={3} current={1} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should have 5 pages, page 5 active and display pages 3 to 5', () => {
-    const component = renderer.create(<Pagination total={5} current={5} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination total={5} current={5} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should not display page numbers', () => {
-    const component = renderer.create(<Pagination showPrevNext delta={0} total={5} current={5} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination showPrevNext delta={0} total={5} current={5} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should not display Previous/Next buttons', () => {
-    const component = renderer.create(<Pagination delta={3} total={5} current={2} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination delta={3} total={5} current={2} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('Next button should be disabled', () => {
-    const component = renderer.create(<Pagination total={3} current={3} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination total={3} current={3} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should not render if total pages equals 1', () => {
-    const component = renderer.create(<Pagination total={1} current={1} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination total={1} current={1} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should not render if current page is greater than total pages', () => {
-    const component = renderer.create(<Pagination total={2} current={3} />);
-    expect(component.toJSON()).toMatchSnapshot();
+    const { asFragment } = render(<Pagination total={2} current={3} />);
+    expect(asFragment()).toMatchSnapshot();
   });
   it('should call to onChange event on click on navigation button', () => {
     const onChange = jest.fn();
-    const component = mount(<Pagination total={5} current={0} showPrevNext onChange={onChange} />);
-    component.find('a.pagination-next').simulate('click');
+    const { container } = render(<Pagination total={5} current={0} showPrevNext onChange={onChange} />);
+    fireEvent.click(container.querySelector('a.pagination-next'));
     expect(onChange).toHaveBeenCalledTimes(1);
-    component.find('a.pagination-previous').simulate('click');
+    fireEvent.click(container.querySelector('a.pagination-previous'));
     expect(onChange).toHaveBeenCalledTimes(2);
-    component
-      .find('a.pagination-link')
-      .at(1)
-      .simulate('click');
+    fireEvent.click(container.querySelectorAll('a.pagination-link')[1]);
     expect(onChange).toHaveBeenCalledTimes(3);
   });
 });
