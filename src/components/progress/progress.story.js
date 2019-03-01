768e813e5f55d374380786dbc0ad1c415072f09c
John Benavides <john@hireflix.com>
2019-03-01 14:10:55 +0100

    Add example of progress without value (0.7.3)

--- src/components/progress/progress.story.js
+++ src/components/progress/progress.story.js
@@ -7,6 +7,7 @@ import Progress from 'react-bulma-components/lib/components/progress';
 storiesOf('Progress', module)
   .add('Default', (() => (
     <div>
+      <Progress />
       <Progress max={100} value={15} color="primary" size="small" />
       <Progress max={100} value={15} color="info" />
       <Progress max={100} value={15} color="success" size="medium" />
