32c656be9943e6dd42137fb24d33a3f3732addcc
John Benavides <john@hireflix.com>
2019-03-02 13:15:38 +0100

    add react-router

--- public/src/App.js
+++ public/src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from 'layout';
-
 import 'helpers.scss';
+
+// import Index from './screens/index';
+// import GettingStarted from './screens/getting-started';
+
+const Index = React.lazy(() => import('./screens/index'));
+const GettingStarted = React.lazy(() => import('./screens/getting-started'));
+
+function WaitingComponent(Component) {
+  return props => (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component {...props} />
+    </Suspense>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <div> loonng</div>
-      </Layout>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={WaitingComponent(Index)} />
+            <Route path="/getting-started/" component={WaitingComponent(GettingStarted)} />
+          </Switch>
+        </Layout>
+      </Router>
     );
   }
 }
