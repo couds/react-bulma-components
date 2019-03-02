8b34529da34460d02e949a57b465459a07fbcb5a
John Benavides <john@hireflix.com>
2019-03-02 13:15:38 +0100

    add react-router

--- public/src/layout/layout.js
+++ public/src/layout/layout.js
@@ -4,19 +4,22 @@ import Navbar from 'react-bulma-components/lib/components/navbar';
 import Container from 'react-bulma-components/lib/components/container';
 import Hero from 'react-bulma-components/lib/components/hero';
 import logo from 'images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Layout = ({ children }) => (
   <div className="app">
     <Navbar fixed="top">
     <Container>
-      <Navbar.Brand className="is-vertically-centered">
-        <img src={logo} alt="logo" style={{ height: 48 }} />
-        <Element textColor="primary">
-          RBC
-        </Element>
+      <Navbar.Brand>
+        <Link to="/"  className="is-vertically-centered">
+          <img src={logo} alt="logo" style={{ height: 48 }} />
+          <Element textColor="primary">
+            RBC
+          </Element>
+        </Link>
       </Navbar.Brand>
       <Navbar.Container position="end">
-        <Navbar.Item>
+        <Navbar.Item renderAs={Link} to="/getting-started/">
           Getting Started
         </Navbar.Item>
       </Navbar.Container>
---
+++ public/src/screens/index/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    Index
+  </div>
+);
+
+export default Index;
