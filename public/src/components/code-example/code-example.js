0ea0b2f0116ee75e43402dcd8abff8cab3d384fe
John Benavides <john@hireflix.com>
2019-03-04 12:31:40 +0100

    improve home

--- public/src/components/code-example/code-example.js
+++ public/src/components/code-example/code-example.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import Hightlight from 'react-highlight';
 import Columns from 'react-bulma-components/lib/components/columns';
 import Button from 'react-bulma-components/lib/components/button';
 
+import { Prism as Hightlight } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 import { FormattedMessage, defineMessages } from 'react-intl';
+
+import './code-example.scss';
 
 const messages = defineMessages({
   toggle: {
@@ -15,19 +19,19 @@ const messages = defineMessages({
 const CodeExample = ({ children, reactCode, bulmaCode, style, size }) => {
   const [current, setCurrent] = useState('react'); // react or bulma
   return (
-    <div style={{ overflow: 'hidden', position: 'relative', ...style }}>
+    <div className="code-example" style={{ overflow: 'hidden', position: 'relative', ...style }}>
       <Columns gapless>
         <Columns.Column size={size} className="is-vertically-centered">
           {children}
         </Columns.Column>
-        <Columns.Column>
+        <Columns.Column className="code"  size={12 -size} >
           <Button 
           size="small"
           style={{ position: 'absolute', top: 0, right: 0}}
           onClick={() => setCurrent(current === 'react' ? 'bulma' : 'react')}>
             <FormattedMessage {...messages.toggle} />
           </Button>
-          <Hightlight language="jsx">
+          <Hightlight language={current === 'react' ? 'jsx' : 'html'} style={atomDark}>
             {(current === 'react' ? reactCode : bulmaCode).trim()}
           </Hightlight>
         </Columns.Column>
@@ -38,6 +42,8 @@ const CodeExample = ({ children, reactCode, bulmaCode, style, size }) => {
 
 CodeExample.defaultProps = {
   size: 6,
+  reactCode: '',
+  bulmaCode: '',
 };
 
 export default CodeExample;
