510040b8ab7ec4cb8c556252c0113958d86e8613
John Benavides <john@hireflix.com>
2019-03-04 13:14:31 +0100

    correctly remove hightlight.js

--- public/src/components/code-example/code-example.js
+++ public/src/components/code-example/code-example.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import Columns from 'react-bulma-components/lib/components/columns';
 import Button from 'react-bulma-components/lib/components/button';
 
-import { Prism as Hightlight } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Highlight from 'components/highlight';
 
 import { FormattedMessage, defineMessages } from 'react-intl';
 
@@ -16,7 +15,7 @@ const messages = defineMessages({
   },
 });
 
-const CodeExample = ({ children, reactCode, bulmaCode, style, size }) => {
+const CodeExample = ({ children, reactCode, bulmaCode, style, size, codeMaxHeight }) => {
   const [current, setCurrent] = useState('react'); // react or bulma
   return (
     <div className="code-example" style={{ overflow: 'hidden', position: 'relative', ...style }}>
@@ -24,16 +23,16 @@ const CodeExample = ({ children, reactCode, bulmaCode, style, size }) => {
         <Columns.Column size={size} className="is-vertically-centered">
           {children}
         </Columns.Column>
-        <Columns.Column className="code"  size={12 -size} >
+        <Columns.Column className="code"  size={12 -size} style={{ maxHeight: codeMaxHeight, overflowY: 'scroll' }} >
           <Button 
           size="small"
           style={{ position: 'absolute', top: 0, right: 0}}
           onClick={() => setCurrent(current === 'react' ? 'bulma' : 'react')}>
             <FormattedMessage {...messages.toggle} />
           </Button>
-          <Hightlight language={current === 'react' ? 'jsx' : 'html'} style={atomDark}>
+          <Highlight language={current === 'react' ? 'jsx' : 'html'}>
             {(current === 'react' ? reactCode : bulmaCode).trim()}
-          </Hightlight>
+          </Highlight>
         </Columns.Column>
       </Columns>
     </div>
