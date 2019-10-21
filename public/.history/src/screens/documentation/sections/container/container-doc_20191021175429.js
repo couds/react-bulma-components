6256a3a4f299299a2a01368f11b87de909cca64b
Kenneth <ngcreeper@outlook.com>
2019-10-21 19:33:30 -0400

    Removed .history folder in public folder

--- public/.history/src/screens/documentation/sections/container/container-doc_20191021175429.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ContainerDoc = () => (
-	<div>Container</div>
-);
-
-export default ContainerDoc;
--- public/.history/src/screens/documentation/sections/container/container-doc_20191021185226.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Heading } from 'react-bulma-components';
-
-const ContainerDoc = () => (
-	<>
-		</>
-);
-
-export default ContainerDoc;
