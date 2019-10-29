5e09eafd2b6cb4e87d965acb53d2a5e41f25f678
Kenneth Ng <kennethng@Kenneths-MacBook-Pro.local>
2019-10-29 18:07:16 -0400

    Fixed icon.js propTypes error

--- public/src/components/icon/icon.js
+++ public/src/components/icon/icon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import BIcon from 'react-bulma-components/lib/components/icon';
 import './icon.scss';
 
@@ -7,5 +8,15 @@ const Icon = ({ icon, ...props }) => (
     <i className={`icon-${icon}`} />
   </BIcon>
 );
+
+BIcon.propTypes = {
+  ...BIcon.propTypes,
+  size: PropTypes.oneOf(['small', 'medium', 'large', ...Array(24).keys()])
+};
+
+Icon.propTypes = {
+  ...BIcon.propTypes,
+  icon: PropTypes.string,
+};
 
 export default Icon;
