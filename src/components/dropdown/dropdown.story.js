0dab6c0c5aed899f8a27f953e9f33dedb8c5bb08
MrCreeper1008 <ngcreeper@outlook.com>
2020-12-16 02:00:36 +0000

    WIP: add dropdown doc

--- src/components/dropdown/dropdown.story.js
+++ src/components/dropdown/dropdown.story.js
@@ -11,6 +11,28 @@ const alignment = {
   right: 'right',
 };
 
+export const Default = () => (
+  <Container fluid>
+    <Dropdown onChange={action('select')}>
+      <Dropdown.Item value="item">Dropdown item</Dropdown.Item>
+      <Dropdown.Item value="other">Other Dropdown item</Dropdown.Item>
+      <Dropdown.Item value="active">Active Dropdown item</Dropdown.Item>
+      <Dropdown.Item value="other 2">Other Dropdown item</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item value="divider">With divider</Dropdown.Item>
+    </Dropdown>
+  </Container>
+);
+
+Default.argTypes = {
+  label: {
+    control: {
+      default: 'label',
+      type: 'text',
+    },
+  },
+};
+
 storiesOf('Dropdown', module)
   .add('Default', () => (
     <Dropdown onChange={action('select')} label={text('label', '')}>
