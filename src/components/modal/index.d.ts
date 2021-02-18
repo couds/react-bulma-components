01825fbda42b7881042a25db5531fb663a7473fb
David Smith <david.pw.smith@posteo.org>
2021-02-18 17:34:51 +0000

    Revert "Fix missing Modal ts props"
    This reverts commit f2e1a0a98d44e8dca0a6fc0cdcced9d9dac6f7f1.

--- src/components/modal/index.d.ts
+++ src/components/modal/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BulmaComponent } from '..';
+import { BulmaComponent, BulmaComponentWithoutModifiers } from '..';
 
 interface ModalProps {
   show?: boolean;
@@ -8,9 +8,6 @@ interface ModalProps {
   closeOnBlur?: boolean;
   showClose?: boolean;
   domRef?: React.RefObject<HTMLDivElement>;
-  document?: Document;
-  children: React.ReactNode;
-  className?: string;
 }
 
 interface ModalCardHeadProps {
@@ -18,9 +15,7 @@ interface ModalCardHeadProps {
   onClose?: () => void;
 }
 
-export const Modal: (
-  props: ModalProps,
-) => React.ReactElement & {
+export const Modal: BulmaComponentWithoutModifiers<ModalProps> & {
   Content: BulmaComponent<{}, 'div'>;
   Card: BulmaComponent<{}, 'div'> & {
     Head: BulmaComponent<ModalCardHeadProps, 'header'>;
