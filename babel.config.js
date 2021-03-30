b4f084085246ce3caa4eaf2a7332a0c3fbecc48f
Kenneth <kenneth.ng.5226@outlook.com>
2021-03-30 18:54:04 +0100

    Enable classProperties in babel

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,6 @@ module.exports = (api) => {
       '@babel/preset-react',
       ['@babel/preset-env', { modules: isTestEnv ? 'commonjs' : false }],
     ],
-    plugins: isTestEnv ? ['@babel/plugin-proposal-class-properties'] : [],
+    plugins: ['@babel/plugin-proposal-class-properties'],
   };
 };
