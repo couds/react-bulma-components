744901e4de75f9960906a4dd8ba864ea489157d4
John Benavides <john.elric@gmail.com>
2021-04-01 11:56:28 +0200

    Message docs

--- src/components/message/message.story.js
+++ src/components/message/message.story.js
@@ -1,13 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Message, Button } from '../../index';
 import CONSTANTS from '../../constants';
 
-export const BasicUsage = (args) => (
+export const BasicUsage = ({ withoutHeader, ...args }) => (
   <Message {...args}>
-    <Message.Header>
-      Title
-      <Button remove />
-    </Message.Header>
+    {!withoutHeader && (
+      <Message.Header>
+        Title
+        <Button remove />
+      </Message.Header>
+    )}
     <Message.Body>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.{' '}
       <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec
@@ -37,56 +40,10 @@ BasicUsage.argTypes = {
       options: ['small', 'medium', 'large'],
     },
   },
-};
-
-export const DifferentColors = () => (
-  <>
-    <Message color="info">
-      <Message.Header>
-        Title
-        <Button remove />
-      </Message.Header>
-      <Message.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{' '}
-        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta
-        nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida
-        purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac{' '}
-        <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-        sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna
-        a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-      </Message.Body>
-    </Message>
-    <Message color="danger">
-      <Message.Header>
-        Title
-        <Button remove />
-      </Message.Header>
-      <Message.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{' '}
-        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta
-        nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida
-        purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac{' '}
-        <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-        sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna
-        a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-      </Message.Body>
-    </Message>
-  </>
-);
-
-export const BodyOnly = (args) => (
-  <Message {...args}>
-    <Message.Body>This is some profound, inspirational message.</Message.Body>
-  </Message>
-);
-
-BodyOnly.argTypes = {
-  color: {
-    description: 'Color of the message block',
-    defaultValue: '',
+  withoutHeader: {
+    name: 'Hide Header',
     control: {
-      type: 'select',
-      options: Object.values(CONSTANTS.COLORS),
+      type: 'boolean',
     },
   },
 };
