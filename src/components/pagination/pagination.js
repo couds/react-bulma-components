ae2e4bbce6998a2760ef463407e6a33761673296
John Benavides <john.elric@gmail.com>
2021-04-02 21:08:41 +0200

    refactor pagination to functional component

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,190 +1,163 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { normalizeAlign } from '../../services/normalizer';
 
 import Element from '../element';
 
-const normalizeAlign = (align) => {
-  if (align === 'center') {
-    return 'centered';
+const getFirstPage = (current, last, delta) => {
+  if (current === 1) {
+    return 1;
   }
-  if (align === 'justify') {
-    return 'justifyed';
-  }
-  return align;
+
+  const minPage = last - delta * 2;
+  const page = Math.min(current - delta, minPage);
+
+  return page <= 0 ? 1 : page;
 };
 
-export default class Pagination extends React.PureComponent {
-  goToPage = (page) => (evt) => {
-    evt.preventDefault();
-    const { onChange } = this.props;
-    onChange(page);
-  };
-
-  firstPage = (current, last) => {
-    const { delta } = this.props;
-
-    if (current === 1) {
-      return 1;
-    }
-
-    const minPage = last - delta * 2;
-    const page = Math.min(current - delta, minPage);
-
-    return page <= 0 ? 1 : page;
-  };
-
-  lastPage = (current, total) => {
-    const { delta } = this.props;
-
-    if (current === total) {
-      return total;
-    }
-
-    const maxPage = delta * 2 + 1;
-    const page = Math.max(current + delta, maxPage);
-    return page > total ? total : page;
-  };
-
-  render() {
-    const {
-      current,
-      disabled,
-      total,
-      next,
-      previous,
-      showPrevNext,
-      showFirstLast,
-      delta,
-      autoHide,
-      className,
-      size,
-      position,
-      rounded,
-      onChange,
-      ...props
-    } = this.props;
-    if (total <= 1 && autoHide) {
-      return null;
-    }
-
-    if (current > total) {
-      return null;
-    }
-
-    const lastPage = this.lastPage(current, total);
-    const firstPage = this.firstPage(current, lastPage);
-    const prevDisabled = current === 1 || disabled;
-    const nextDisabled = current === total || disabled;
-
-    return (
-      <Element
-        {...props}
-        className={classnames('pagination', className, {
-          [`is-${size}`]: size,
-          [`is-${normalizeAlign(position)}`]: position,
-          'is-rounded': rounded,
-        })}
-        aria-label="pagination"
-      >
-        {showPrevNext && (
-          <>
-            <a
-              role="button"
-              tabIndex={0}
-              onClick={prevDisabled ? undefined : this.goToPage(current - 1)}
-              className="pagination-previous"
-              title="This is the first page"
-              disabled={prevDisabled}
-            >
-              {previous}
-            </a>
-            <a
-              role="button"
-              tabIndex={0}
-              onClick={nextDisabled ? undefined : this.goToPage(current + 1)}
-              className="pagination-next"
-              disabled={nextDisabled}
-            >
-              {next}
-            </a>
-          </>
-        )}
-        {delta > 0 && (
-          <>
-            <ul className="pagination-list">
-              {showFirstLast && current !== 1 && firstPage !== 1 && (
-                <>
-                  <li key={1}>
-                    <a
-                      role="button"
-                      tabIndex={0}
-                      className="pagination-link"
-                      onClick={this.goToPage(1)}
-                      aria-label="Page 1"
-                      aria-current="page"
-                    >
-                      1
-                    </a>
-                  </li>
-                  <li>
-                    <span className="pagination-ellipsis">&hellip;</span>
-                  </li>
-                </>
-              )}
-              {Array(lastPage - firstPage + 1)
-                .fill(0)
-                .map((_, i) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <li key={i + firstPage}>
-                    <a
-                      role="button"
-                      tabIndex={0}
-                      className={classnames('pagination-link', {
-                        'is-current': current === i + firstPage,
-                      })}
-                      onClick={
-                        current === firstPage + i
-                          ? undefined
-                          : this.goToPage(firstPage + i)
-                      }
-                      aria-label={`Page ${i + firstPage}`}
-                      aria-current="page"
-                      disabled={disabled}
-                    >
-                      {i + firstPage}
-                    </a>
-                  </li>
-                ))}
-              {showFirstLast && current !== lastPage && total !== lastPage && (
-                <>
-                  <li key={total}>
-                    <span className="pagination-ellipsis">&hellip;</span>
-                  </li>
-                  <li>
-                    <a
-                      role="button"
-                      tabIndex={0}
-                      className="pagination-link"
-                      onClick={this.goToPage(total)}
-                      aria-label={`Page ${total}`}
-                      aria-current="page"
-                    >
-                      {total}
-                    </a>
-                  </li>
-                </>
-              )}
-            </ul>
-          </>
-        )}
-      </Element>
-    );
+const getLastPage = (current, total, delta) => {
+  if (current === total) {
+    return total;
   }
-}
+
+  const maxPage = delta * 2 + 1;
+  const page = Math.max(current + delta, maxPage);
+  return page > total ? total : page;
+};
+
+const Pagination = ({
+  current,
+  disabled,
+  total,
+  next,
+  previous,
+  showPrevNext,
+  showFirstLast,
+  delta,
+  autoHide,
+  className,
+  size,
+  position,
+  rounded,
+  onChange,
+  ...props
+}) => {
+  if ((total <= 1 && autoHide) || current > total) {
+    return null;
+  }
+
+  const lastPage = getLastPage(current, total, delta);
+  const firstPage = getFirstPage(current, lastPage, delta);
+  const prevDisabled = current === 1 || disabled;
+  const nextDisabled = current === total || disabled;
+
+  return (
+    <Element
+      {...props}
+      className={classnames('pagination', className, {
+        [`is-${size}`]: size,
+        [`is-${normalizeAlign(position)}`]: position,
+        'is-rounded': rounded,
+      })}
+      aria-label="pagination"
+    >
+      {showPrevNext && (
+        <>
+          <a
+            role="button"
+            tabIndex={0}
+            onClick={prevDisabled ? undefined : () => onChange(current - 1)}
+            className="pagination-previous"
+            title="This is the first page"
+            disabled={prevDisabled}
+          >
+            {previous}
+          </a>
+          <a
+            role="button"
+            tabIndex={0}
+            onClick={nextDisabled ? undefined : () => onChange(current + 1)}
+            className="pagination-next"
+            disabled={nextDisabled}
+          >
+            {next}
+          </a>
+        </>
+      )}
+      {delta > 0 && (
+        <>
+          <ul className="pagination-list">
+            {showFirstLast && current !== 1 && firstPage !== 1 && (
+              <>
+                <li key={1}>
+                  <a
+                    role="button"
+                    tabIndex={0}
+                    className="pagination-link"
+                    onClick={() => onChange(1)}
+                    aria-label="Page 1"
+                    aria-current="page"
+                  >
+                    1
+                  </a>
+                </li>
+                <li>
+                  <span className="pagination-ellipsis">&hellip;</span>
+                </li>
+              </>
+            )}
+            {Array(lastPage - firstPage + 1)
+              .fill(0)
+              .map((_, i) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <li key={i + firstPage}>
+                  <a
+                    role="button"
+                    tabIndex={0}
+                    className={classnames('pagination-link', {
+                      'is-current': current === i + firstPage,
+                    })}
+                    onClick={
+                      current === firstPage + i
+                        ? undefined
+                        : () => onChange(firstPage + i)
+                    }
+                    aria-label={`Page ${i + firstPage}`}
+                    aria-current="page"
+                    disabled={disabled}
+                  >
+                    {i + firstPage}
+                  </a>
+                </li>
+              ))}
+            {showFirstLast && current !== lastPage && total !== lastPage && (
+              <>
+                <li key={total}>
+                  <span className="pagination-ellipsis">&hellip;</span>
+                </li>
+                <li>
+                  <a
+                    role="button"
+                    tabIndex={0}
+                    className="pagination-link"
+                    onClick={() => onChange(total)}
+                    aria-label={`Page ${total}`}
+                    aria-current="page"
+                  >
+                    {total}
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
+        </>
+      )}
+    </Element>
+  );
+};
 
 Pagination.propTypes = {
-  ...Element.propTypes,
   /** Current page */
   current: PropTypes.number,
   /** whether to disable the buttons */
@@ -201,17 +174,12 @@ Pagination.propTypes = {
   showPrevNext: PropTypes.bool,
   showFirstLast: PropTypes.bool,
   autoHide: PropTypes.bool,
-  /**
-   * Classname of the container of the pagination, this could be used to customize the inner views
-   */
-  className: PropTypes.string,
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   position: PropTypes.oneOf(['center', 'right']),
   rounded: PropTypes.bool,
 };
 
 Pagination.defaultProps = {
-  ...Element.defaultProps,
   total: 1,
   current: 1,
   delta: 1,
@@ -221,3 +189,5 @@ Pagination.defaultProps = {
   showPrevNext: true,
   autoHide: true,
 };
+
+export default Pagination;
