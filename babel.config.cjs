5e4f9704fb571c18a00977fd489d489a3bfc78c1
John Benavides <john.benavides@gartner.com>
2021-04-21 11:54:47 +0200

    fix imports on TS (Gatsby)

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,21 +1,14 @@
-module.exports = (api) => {
-  // import statements must be compiled in test env
-  // in other environments, modules must be set to false to enable tree shaking
-  // more info: https://jestjs.io/docs/en/getting-started#using-babel
-
-  const useESModules = !api.env(['test']);
-  return {
-    plugins: ['babel-plugin-transform-react-remove-prop-types'],
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          bugfixes: true,
-          debug: process.env.RBC_BUILD_VERBOSE === 'true',
-          modules: useESModules ? false : 'commonjs',
-        },
-      ],
-      ['@babel/preset-react'],
+module.exports = {
+  plugins: ['babel-plugin-transform-react-remove-prop-types'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        bugfixes: true,
+        debug: process.env.RBC_BUILD_VERBOSE === 'true',
+        modules: 'commonjs',
+      },
     ],
-  };
+    ['@babel/preset-react'],
+  ],
 };
